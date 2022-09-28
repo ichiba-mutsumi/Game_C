b18080f20285c99cd228a175f4051aa48c2e67e0
ichiba <urata.9489@gmail.com>
2022-09-28 12:17:23 +0900

    teisei

--- Game03/Project/GameProject/Game/Game.cpp
+++ Game03/Project/GameProject/Game/Game.cpp
@@ -1,7 +1,7 @@
 #include "Game.h"
 #include "Field.h"
 #include "Player.h"
-#include "Enemy.h"
+//#include "Enemy.h"
 #include "Goal.h"
 #include "Map.h"
 #include "../Title/Title.h"
@@ -10,10 +10,10 @@ Game::Game() :Base(eType_Scene)
 {
 	//Base::Add(new Field());
 	Base::Add(new Map());
-	Base::Add(new Player(CVector2D(0, 540), false));
-	Base::Add(new Enemy(CVector2D(1280 + 256 * 1, 540), true));
-	Base::Add(new Enemy(CVector2D(1280 + 256 * 2, 540), true));
-	Base::Add(new Enemy(CVector2D(1280 + 256 * 3, 540), true));
+	Base::Add(new Player(CVector2D(1280, 540), false));
+	//Base::Add(new Enemy(CVector2D(1280 + 256 * 1, 540), true));
+	//Base::Add(new Enemy(CVector2D(1280 + 256 * 2, 540), true));
+	//Base::Add(new Enemy(CVector2D(1280 + 256 * 3, 540), true));
 	Base::Add(new Goal(CVector2D(2048, 540)));
 
 }
